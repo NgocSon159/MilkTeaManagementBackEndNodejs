a262dc927321faec02abcf22d9dc668d133e05ba
Ngoc Son <son.95.1112@gmail.com>
2019-09-28 09:58:49 +0700

    Change data & script mongo

--- data/init-data-MilkTea-Mongo.bat
+++ data/init-data-MilkTea-Mongo.bat
@@ -1,5 +1,11 @@
+mongo MilkTea --eval "db.dropDatabase()"
+
 mongoimport --host localhost --db MilkTea --collection Food --type json --file ./food_init.json --jsonArray --maintainInsertionOrder
 
 mongoimport --host localhost --db MilkTea --collection Categories --type json --file ./categories_init.json --jsonArray --maintainInsertionOrder
 
 mongoimport --host localhost --db MilkTea --collection Orders --type json --file ./orders_init.json --jsonArray --maintainInsertionOrder
+
+mongoimport --host localhost --db MilkTea --collection Table --type json --file ./table_init.json --jsonArray --maintainInsertionOrder
+
+mongoimport --host localhost --db MilkTea --collection Type --type json --file ./type_init.json --jsonArray --maintainInsertionOrder
---
+++ data/remove database.bat
@@ -0,0 +1,1 @@
+mongo MilkTea --eval "db.dropDatabase()"
